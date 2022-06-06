c0580fbdfcc9117489db5f39d594d5021c497c4f
Hamdamboy Urunov <hamdamboy@Hamdamboys-MacBook-Pro.local>
2022-06-06 12:44:02 +0500

    Java Fundamental

---
+++ JavaCore/JavaProject-0/FirstHelloWorld/src/MathPractice.java
@@ -0,0 +1,33 @@
+public class MathPractice {
+    public static void main(String[]args){
+
+        String name="Husniddin"; // type - tip
+        String className="Java Backend.";
+        int age = 32;
+        int position= 5;
+
+        System.out.println("Name: " + name + "  " + "className" + className);
+        System.out.println("--------");
+        System.out.println("Name: " + name + " my age: " + age + "  my position:  " + position);
+
+        int num1 = 109;
+        int num2 = 201;
+        int sum = 0;
+
+        System.out.println("SUMMA: " + sum);
+
+        // step-0: Adding two numbers
+        sum = num1 + num2;
+        System.out.println("SUMMA1: " + sum);
+        // step-1: Adding two numbers inside sout.
+        System.out.println("SUMMA2: " + (num1 + num2));
+        //step-2: Adding two numbers .
+        System.out.println("SUMMA3: " + num1 + num2);
+
+        // step-3. Boolean Type.
+        boolean tellMe = false;
+        System.out.println("TELL ME: " + tellMe);
+    }
+}
+
+// Double, boolean, float, char.
---
+++ JavaCore/JavaProject-0/FirstHelloWorld/src/MyProjectType.java
@@ -0,0 +1,9 @@
+public class MyProjectType {
+    public static void main(String[] args) {
+
+        double number = 4.3;
+        double num2 =5;
+
+        
+    }
+}
